d7cc157384f7c3b8edbb8c6d5c31c21a6273a018
Star-Rock <samuelsturrock@outlook.com>
2020-12-12 00:37:37 +0000

    Add files via upload

---
+++ Header.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+
+	<head>
+		<link rel="stylesheet" href="css.css">
+	<head>
+
+	<body>	
+	
+	<?php 
+	#$HeaderMain = "HeaderImages/placeholder.jpeg";
+	$Header = "	
+		<center>
+			<nav>
+				<a href=\"home.html\"><img class = \"logo\" src=\"HeaderImages/logo.png\"/></a>
+				<a class =\"nav\" href=\"home.html\">Home |</a>
+				<a class =\"nav\" href=\"mountain.html\">Mountain  |</a>
+				<a class =\"nav\" href=\"Contact.html\">Contact  |</a>
+				<a class =\"nav\" href=\"About.html\">About  |</a>
+				<input type=\"text\" placeholder=\"Search..\">
+			</nav>
+		</center>
+			<img id =\"HeaderMain\" src=\"$HeaderMain\">
+			<img id =\"ImageCover\" src=\"HeaderImages/headerend.png\">
+			"; ?>
+			
+		
+		
+		
+
+
+
+	</body>
+
+</html>
---
+++ Home.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+	<head><title>Home</title></head>
+	
+	<body>
+
+<?php 
+$HeaderMain = "HeaderImages/placeholder2.jpg";
+include "header.php";
+echo $Header
+
+?>
+
+	</body>
+	
+</html>
